446cc7cfc0abe9cf85f8207b9ea1692abb3eb3c6
Janek Kolanek <139225502+janekkolanek@users.noreply.github.com>
2025-04-05 10:33:14 +0200

    Update config.py

--- geekoteka-core/application/config.py
+++ geekoteka-core/application/config.py
@@ -1,2 +1,2 @@
-EMAIL_ADDRESS = 'pytmailbot@gmail.com'
-EMAIL_PASSWORD = 'xerv csgy vpwo gkhk'       # Hasło aplikacji (wygenerowane w ustawieniach Gmaila dla aplikacji zewnętrznych)
+EMAIL_ADDRESS = 'MAIL'
+EMAIL_PASSWORD = 'GMAIL TOKEN'       # Hasło aplikacji (wygenerowane w ustawieniach Gmaila dla aplikacji zewnętrznych)
